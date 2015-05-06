9bf7b02549c3893ba5c6a59f34c41ccb25a1fbd9
Raimondo Butera <rai@rbutera.com>
2015-05-06 05:55:48 +0100

    add project methods

--- common/models/project.js
+++ common/models/project.js
@@ -1,3 +1,81 @@
-module.exports = function(Project) {
+module.exports = function (Project) {
+    Project.listProjects = function listProjects(cb) {
+        Project.find({
+            fields: {
+                balance: false
+            }
+        }, cb);
+    };
 
+    Project.remoteMethod('listProjects', {
+        returns: {
+            arg: 'projects',
+            type: 'array'
+        },
+        http: {
+            path: '/list-projects',
+            verb: 'get'
+        }
+    });
+
+
+
+    Project.donate = function (id, amount, cb) {
+        Project.findById(id, function (err, project) {
+            if (err) return cb(err);
+
+            project.balance += amount;
+            project.save();
+
+            cb(null, true);
+        });
+    };
+
+    Project.remoteMethod('donate', {
+        accepts: [{
+            arg: 'id',
+            type: 'number'
+        }, {
+            arg: 'amount',
+            type: 'number'
+        }],
+        returns: {
+            arg: 'success',
+            type: 'boolean'
+        },
+        http: {
+            path: '/donate',
+            verb: 'post'
+        }
+    });
+
+    Project.withdraw = function withdraw(id, amount, cb) {
+        Project.findById(id, function (err, project) {
+            if (err) return cb(err);
+
+            project.balance = project.balance >= amount ?
+                project.balance - amount : 0;
+            project.save();
+
+            cb(null, true);
+        });
+    };
+
+    Project.remoteMethod('withdraw', {
+        accepts: [{
+            arg: 'id',
+            type: 'number'
+        }, {
+            arg: 'amount',
+            type: 'number'
+        }, ],
+        returns: {
+            arg: 'success',
+            type: 'boolean'
+        },
+        http: {
+            path: '/withdraw',
+            verb: 'post'
+        }
+    });
 };
